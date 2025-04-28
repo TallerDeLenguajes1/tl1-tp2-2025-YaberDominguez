6c26d9d2023862d64a0f02754c271cb20c118370
Yaber Dominguez <yaberdamianmariadominguezbaez@gmail.com>
2025-04-28 13:18:09 -0300

    Añado bucle for para generar las caracteristicas de las 5 computadoras

--- tp2_4.c
+++ tp2_4.c
@@ -10,5 +10,24 @@ typedef struct
 } compu;
 
 int generar_valores_aleatorios(int limite_inferior, int limite_superior){
-    int numero = 
+    /*Genero un numero aleatorio con rand y pongo el intervalo */
+    int numero = limite_inferior + rand() % (limite_superior - limite_inferior + 1);
+    return numero;
+}
+
+char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core",
+    "Pentium"};
+
+int main(){
+    compu computadoras[5];
+    int indice;
+    for (int i = 0; i < 5; i++)
+    {
+        computadoras[i].velocidad = generar_valores_aleatorios(1,3);
+        computadoras[i].anio = generar_valores_aleatorios(2015,2024);
+        computadoras[i].cantidad_nucleos = generar_valores_aleatorios(1,8);
+        indice = generar_valores_aleatorios(0,5);
+        computadoras[i].tipo_cpu = tipos[indice];
+    }
+    
 }
